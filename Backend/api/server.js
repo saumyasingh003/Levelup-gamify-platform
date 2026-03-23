99d45ac9cce1fcffc34d585606a9ab0ba7b22ea5
saumyasingh003 <saumyasingh98982@gmail.com>
2026-03-23 19:04:01 +0530

    updates

---
+++ Backend/api/server.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "../src/app.js";
+import connectDB from "../src/config/db.js";
+
+export default async function handler(req, res) {
+  await connectDB();  
+  return app(req, res); 
+}
+
+
+
